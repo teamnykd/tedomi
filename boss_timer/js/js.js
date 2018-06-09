1a213d6164c42c6b06f498506ae145a086ef8a0a
teamnykd <teamnykd@users.noreply.github.com>
2018-06-09 23:16:09 +0700

    +
    stable

--- boss_timer/js/js.js
+++ boss_timer/js/js.js
@@ -5,7 +5,7 @@ var interval = setInterval(myFunction, 1000);
 var current_boss =[];
 var accept_notify = false;
 function myFunction2() {	
-	var audio = new Audio('../js/nhac.mp3');
+	var audio = new Audio('boss_timer/js/nhac.mp3');
 	audio.play();
 }
 function myFunction() {	
