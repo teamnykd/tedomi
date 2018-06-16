0b26368d0acd2f9da3245df13ab8bfc995623636
teamnykd <teamnykd@users.noreply.github.com>
2018-06-16 08:34:33 +0700

    add speed

--- game_snake.js
+++ game_snake.js
@@ -22,8 +22,7 @@ function getHeight() {
   );
 }
 
-function startGame() { 
-	//alert(getWidth()+"="+getHeight());
+function startGame() { 	
     myGameArea.init();	
 	gameManager.init();	
 	document.addEventListener("keydown", keydownHandler = function (e) {	
@@ -31,20 +30,37 @@ function startGame() {
 		switch(e.key)
 		{
 			case 'w':
+			case 'W':
 				if(gameManager.direction != 3)
 					gameManager.direction = 1;
 				break;
 			case 's':	
+			case 'S':
 				if(gameManager.direction != 1)
 					gameManager.direction = 3;			
 				break;
 			case 'd':
+			case 'D':
 				if(gameManager.direction != 2)
 					gameManager.direction = 4;
 				break;
 			case 'a':
+			case 'A':
 				if(gameManager.direction != 4)
 					gameManager.direction = 2;
+				break;
+			case 'Shift':		
+					myGameArea.limitLoop(updateGame, myGameArea.fps * 3);
+				break;
+			default: 
+			break;
+		}
+	}, false);
+	document.addEventListener("keyup", keydownHandler = function (e) {
+		switch(e.key)
+		{			
+			case 'Shift':		
+					myGameArea.limitLoop(updateGame, myGameArea.fps);
 				break;
 			default: 
 			break;
@@ -59,20 +75,33 @@ function updateGame() {
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
+	requestID: 0,
+	fps: 20,
     init : function() {	
         this.canvas.width = (Math.floor(getWidth()/10)-2) * 10;
         this.canvas.height = (Math.floor(getHeight()/10)-2) * 10;		
 		//this.canvas.style="border:1px solid #000000;";
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);     
-		this.interval = setTimeout(this.update, 30);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]); 
+		this.limitLoop(updateGame, this.fps);	
     },
-	update: function(){
-		limitLoop(updateGame, 20);
+	limitLoop: function(fn, fps) {	
+		window.cancelAnimationFrame(myGameArea.requestID);		
+		var then = new Date().getTime();
+		// custom fps, otherwise fallback to 60
+		fps = fps || 60;
+		var interval = 1000 / fps;	
+		(function loop(){
+			myGameArea.requestID = requestAnimationFrame(loop);
+			var now = new Date().getTime();
+			var delta = now - then;
+			
+			if (delta > interval) {				
+				then = now - (delta % interval);	 
+				fn();
+			}
+		}());
 	},
-    stop : function() {
-        clearInterval(this.interval);
-    }, 
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }	
@@ -82,7 +111,8 @@ var gameManager = {
 	target: [101,101],
 	direction: 4,
 	init : function() {	
-		this.snake = [[101,1],[91,1],[81,1],[71,1],[61,1],[51,1],[41,1],[31,1],[21,1],[11,1],[1,1]];
+		this.snake = [[11,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]];
+		this.direction = 4;
     },
 	draw: function(node, color)
 	{				
@@ -104,7 +134,7 @@ var gameManager = {
 			myGameArea.context.fill();
 			myGameArea.context.closePath();		
 	},
-	update : function() {
+	update : function() {		
 		let newHead = [this.snake[0][0],this.snake[0][1]];		
 		switch(this.direction)
 		{
@@ -128,15 +158,11 @@ var gameManager = {
 		let b = JSON.stringify(newHead);
 		var c = a.indexOf(b);
 		if(c != -1){
-			audioDie.play();
-			this.direction = 0;
-			died = true;
+			this.end();
 		}		
 		if(newHead[0] <= 0 || newHead[0] >= myGameArea.canvas.width || newHead[1] <= 0 || newHead[1] >= myGameArea.canvas.height)
 		{	
-				audioDie.play();
-				this.direction = 0;
-				died = true;
+			this.end();	
 		}
 		else
 		{		
@@ -155,7 +181,18 @@ var gameManager = {
 		this.draw(this.snake[0], "black");
 		this.draw(this.snake[1], "red");		
 		this.draw(this.target, "blue");
-    }
+    },
+	end: function()
+	{
+		audioDie.play();
+		this.direction = 0;
+		died = true;
+		if(window.confirm("Score: " + (this.snake.length - 10)))
+		{				
+			died = false;		
+			startGame();
+		}
+	}
 }
 
 
@@ -170,34 +207,9 @@ window.requestAnimationFrame = function() {
 			window.setTimeout(f,1e3/60);
 		}
 }();
-var limitLoop = function (fn, fps) {
- 
-    // Use var then = Date.now(); if you
-    // don't care about targetting < IE9
-    var then = new Date().getTime();
-
-    // custom fps, otherwise fallback to 60
-    fps = fps || 60;
-    var interval = 1000 / fps;
- 
-    return (function loop(time){
-        requestAnimationFrame(loop);
- 
-        // again, Date.now() if it's available
-        var now = new Date().getTime();
-        var delta = now - then;
- 
-        if (delta > interval) {
-            // Update time
-            // now - (delta % interval) is an improvement over just 
-            // using then = now, which can end up lowering overall fps
-            then = now - (delta % interval);
- 
-            // call the fn
-            fn();
-        }
-    }(0));
-};
+window.cancelAnimationFrame = window.cancelAnimationFrame
+                              || window.mozCancelAnimationFrame
+                              || function(requestID){clearTimeout(requestID)}
 function random(min, max)
 {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
